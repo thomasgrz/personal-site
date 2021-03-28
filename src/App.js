1e1d32c23e0e8fcbf28831eed630335190c495e2
thomas <thomasgrz@outlook.com>
2021-03-28 16:51:08 -0400

    added React boilerplate

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function App() {
+  return <h1>{document.title}</h1>;
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,44 @@
 const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
-const { ProvidePlugin } = require("webpack");
+const { ProvidePlugin, HotModuleReplacementPlugin } = require("webpack");
 
 module.exports = {
   entry: {
     index: "./src/index.js",
   },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
   mode: "development",
   devtool: "inline-source-map",
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
+    contentBase: path.join(__dirname, "public"),
     compress: true,
     port: 9000,
   },
   output: {
-    filename: "[name].js",
+    filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
   plugins: [
+    new HotModuleReplacementPlugin(),
     new ProvidePlugin({ process: "process/browser" }),
-    new CopyPlugin({
-      patterns: [
-        {
-          from: path.join(__dirname, "src/assets"),
-          to: path.join(__dirname, "dist/assets"),
-        },
-      ],
-    }),
+    // new CopyPlugin({
+    //   patterns: [
+    //     {
+    //       from: path.join(__dirname, "src/assets"),
+    //       to: path.join(__dirname, "dist/assets"),
+    //     },
+    //   ],
+    // }),
   ],
 };
